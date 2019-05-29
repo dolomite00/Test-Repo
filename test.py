8850c9bcae33aca076c0149e7847c79c2aae0f8b
Joon S. Lee <dolomite00@gmail.com>
2019-05-29 16:28:59 -0400

    removed some codes

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
 
-def greet(who):
-    test = "Test"
-    greeting = "Hello, {}".format(who)
-    return greeting
-
-
-# print(sys.version)
-# print(sys.executable)
 r = requests.get("https://coreyms.com")
 print(r.status_code)
